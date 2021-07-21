7b0dfed58d738fb3c91f36140dd5deff5cf30994
Daniel Smarda <danieljsmarda@gmail.com>
2021-07-21 17:23:33 +0200

    Replace double quotes with single quotes

--- tweets/get_tweets_requests.py
+++ tweets/get_tweets_requests.py
@@ -1,16 +1,16 @@
 import requests
 import json
 
-settings_file = json.load(open("tweets/settings.json"))
+settings_file = json.load(open('tweets/settings.json'))
 # Replace your bearer token below
-bearer_token = settings_file["bearer_token"]
+bearer_token = settings_file['bearer_token']
 cookie = settings_file['cookie']
 max_results = 10
-start_time = "2020-06-11T16:05:06.000Z"
-end_time = "2020-06-11T16:20:00.000Z"
-query = "trump"
+start_time = '2020-06-11T16:05:06.000Z'
+end_time = '2020-06-11T16:20:00.000Z'
+query = 'trump'
 
-url = f"https://api.twitter.com/2/tweets/search/all?query={query}&start_time={start_time}&end_time={end_time}&max_results={max_results}"
+url = f'https://api.twitter.com/2/tweets/search/all?query={query}&start_time={start_time}&end_time={end_time}&max_results={max_results}'
 
 payload={}
 headers = {
@@ -18,6 +18,6 @@ headers = {
     'Cookie': cookie
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request('GET', url, headers=headers, data=payload)
 
 print(response.text)
