463b5d5b654ad514a83a7ecde22a6a7324ea25c8
Daniel Smarda <danieljsmarda@gmail.com>
2021-08-06 12:33:39 +0200

    Re-add params and base-url that were lost in merge

--- tweets_code/get_tweets.py
+++ tweets_code/get_tweets.py
@@ -1,19 +1,20 @@
-import time
 from request_management import send_n_requests
 from geolocation import geolocate_tweets
 
 dump_path = '../private_data/dump.txt'
 batch_path = '../private_data/batch.txt'
 
-def handle_rate(request_fn):
-    def wrapper(*args, max_time=900, **kwargs):
-        start = time.time()
-        request_fn(*args, **kwargs)
-        end = time.time()
-        elapsed = start - end
-        if elapsed < max_time:
-            time.sleep(max_time - elapsed + 1)
-    return wrapper
+params = {
+    'max_results' : '500', # Results per request
+    'start_time' : '2020-06-11T16:05:06.000Z',
+    'end_time' : '2020-06-11T16:20:00.000Z',
+    'query' : 'trump lang:en',
+    'tweet.fields' : 'author_id',
+    'expansions' : 'author_id',
+    'user.fields' : 'location'
+}
+params_string = '&'.join([key + '=' + value for key, value in params.items()])
+base_url = 'https://api.twitter.com/2/tweets/search/all?' + params_string
 
 def extract_next_token(filename):
     with open(filename, 'r', encoding='utf-16-le') as f:
@@ -25,7 +26,6 @@ def extract_next_token(filename):
 def init_batch():
     for i in range(1):
         next_token = extract_next_token(batch_path)
-        send_n_requests(dump_path, batch_path, next_token=next_token, n=1)
+        send_n_requests(dump_path, batch_path, base_url, next_token=next_token, n=1)
         geolocate_tweets()
 init_batch()
-        
